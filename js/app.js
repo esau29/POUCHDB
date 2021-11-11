9c9aa8d50acc90f46ae5b7375cfe0a708f850406
Esau Roblero <esaur0035@gmail.com>
2021-11-11 15:08:47 -0500

    modificaciones de estilo

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@
 
   // We have to create a new todo document and enter it in the database
   function addTodo(text) {
-    if(text.length==0) return
+    if(text.length ==0) return
     var todo = {
       _id: new Date().toISOString(),
       title: text,
